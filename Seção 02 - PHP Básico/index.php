734845d535ea0a19eb78faa2e45d6609997333dc
nipodemos <nipodemos13@gmail.com>
2019-08-30 01:21:07 -0300

    Operações (Matemáticas)
    ensinou o básico do básico sobre como
    somar
    multiplicar
    dividir
    subtrair
    e exponenciar (esse não sabia)
    variáveis

--- Seção 02 - PHP Básico/index.php
+++ Seção 02 - PHP Básico/index.php
@@ -1,3 +1,19 @@
 <?php
-$mensagem = 'hello world';
-echo $mensagem;
+
+// Aula sobre Operações
+$a = 2;
+$b = 3;
+$soma          = $a + $b;
+$subtracao     = $a - $b;
+$multiplicacao = $a * $b;
+$divisao       = $a / $b;
+$exponenciacao = $a ** $b;
+echo $soma;
+echo $subtracao;
+echo $multiplicacao;
+echo $divisao;
+echo $exponenciacao;
+
+// Aula sobre Variáveis
+// $mensagem = 'hello world';
+// echo $mensagem;
