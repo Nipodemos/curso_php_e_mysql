08f8d3883c8bd659a50a26a6abd8dec4659427f2
nipodemos <nipodemos13@gmail.com>
2019-08-30 01:27:09 -0300

    Operadores Condicionais
    novamente, ensinou o básico de usar a função
    que checa o resto da divisão de um número
    muito usado quando a gente quer descobrir se é par ou ímpar
    
    coisa básica básica mesmo

--- Seção 02 - PHP Básico/index.php
+++ Seção 02 - PHP Básico/index.php
@@ -1,18 +1,28 @@
 <?php
 
+//Aula sobre Operadores Condicionais
+$x = 4;
+$y = 5;
+
+if ($x % 2 === 1) {
+    echo "Número Ímpar";
+} else {
+    echo "Número Par";
+}
+
 // Aula sobre Operações
-$a = 2;
-$b = 3;
-$soma          = $a + $b;
-$subtracao     = $a - $b;
-$multiplicacao = $a * $b;
-$divisao       = $a / $b;
-$exponenciacao = $a ** $b;
-echo $soma;
-echo $subtracao;
-echo $multiplicacao;
-echo $divisao;
-echo $exponenciacao;
+// $a = 2;
+// $b = 3;
+// $soma          = $a + $b;
+// $subtracao     = $a - $b;
+// $multiplicacao = $a * $b;
+// $divisao       = $a / $b;
+// $exponenciacao = $a ** $b;
+// echo $soma;
+// echo $subtracao;
+// echo $multiplicacao;
+// echo $divisao;
+// echo $exponenciacao;
 
 // Aula sobre Variáveis
 // $mensagem = 'hello world';
