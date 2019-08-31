054b8ca9f703869d1edfebbe1540727be463839f
nipodemos <nipodemos13@gmail.com>
2019-08-31 00:56:58 -0300

    Laços de Repetição
    "While" loop
    a condição dentro do parenteses é testada
    e enquando a condição for verdadeira,
    o bloco contina a ser executado
    esse bloco primeiro faz o teste, e se verdadeiro,
    entra no bloco e executa o código lá,
    caso contrário, o conteúdo do bloco é ignorado
    
    O "do .. while" loop é quase igual. A diferença é
    que ele primeiro executa o código, depois faz o teste.
    Isso pode ser importante porque ele sempre
    vai executar o bloco ao menos uma vez
    mesmo que a condição seja falsa de cara.
    
    "For" loop tem uma sintaxe diferente, mas faz a mesma coisa
    basicamente o $i++ que vc sempre tem que colocar no final
    no for vc coloca dentro do próprio parenteses
    Dessa forma fica mais limpa a sintaxe
    
    "Foreach" loop é focado pra dar loop em arrays e hashes
    Ele não ensinou isso ainda mas eu já to
    cansado de saber :D

--- Seção 02 - PHP Básico/index.php
+++ Seção 02 - PHP Básico/index.php
@@ -1,15 +1,43 @@
 <?php
 
-//Aula sobre Operadores Condicionais
-$x = 4;
-$y = 5;
 
-if ($x % 2 === 1) {
-    echo "Número Ímpar";
-} else {
-    echo "Número Par";
+# while loop
+$i = 0;
+while (i < 10) {
+    echo $i;
+    $i++;
 }
 
+#do while loop
+$j = 0;
+do {
+    echo $j;
+    $j++;
+} while (j < 10);
+
+#for loop
+for ($k = 0; $k < 10; $k++) {
+    echo $i;
+}
+
+#foreach
+$i = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+foreach ($l as $value) {
+    echo $value;
+}
+
+
+
+//Aula sobre Operadores Condicionais
+//$x = 4;
+//$y = 5;
+//
+//if ($x % 2 === 1) {
+//    echo "Número Ímpar";
+//} else {
+//    echo "Número Par";
+//}
+//
 // Aula sobre Operações
 // $a = 2;
 // $b = 3;
